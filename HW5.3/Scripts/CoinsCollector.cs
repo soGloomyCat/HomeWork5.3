75fddc32500aeb77cc7b88706cc17acf41b4a9c5
soGloomyCat <91888662+soGloomyCat@users.noreply.github.com>
2021-10-18 11:17:42 +0300

    Add files via upload

---
+++ HW5.3/Scripts/CoinsCollector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinsCollector : MonoBehaviour
+{
+    private int _coinsQuality;
+
+    private void Start()
+    {
+        _coinsQuality = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Coin>(out Coin coin))
+        {
+            _coinsQuality++;
+            Debug.Log($"Собрано монет - {_coinsQuality}");
+        }
+    }
+}
