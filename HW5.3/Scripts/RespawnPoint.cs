c860d3647301c8c0b3f4606b93a5c2ca6dfeb1a2
soGloomyCat <91888662+soGloomyCat@users.noreply.github.com>
2021-10-20 15:36:43 +0300

    Update RespawnPoint.cs

--- HW5.3/Scripts/RespawnPoint.cs
+++ HW5.3/Scripts/RespawnPoint.cs
@@ -7,16 +7,8 @@ public class RespawnPoint : MonoBehaviour
     [SerializeField] private Hero _hero;
     [SerializeField] private GameObject _respawnPoint;
 
-    private void Start()
+    public void Start()
     {
         Instantiate(_hero, _respawnPoint.transform.position, Quaternion.identity);
-    }
-
-    private void Update()
-    {
-        if (!FindObjectOfType<Hero>())
-        {
-            Instantiate(_hero, _respawnPoint.transform.position, Quaternion.identity);
-        }
     }
 }
